e1faceaa40a60606d6a621335fa4fe3201aef2c7
McMarlov <77973979+McMarlov@users.noreply.github.com>
2021-08-02 17:47:36 +0300

    Add files via upload

--- testsqlite3.py
+++ testsqlite3.py
@@ -1,41 +1,44 @@
 import sqlite3
-a = 'Autor'
+a = 'author'
 c = 'Citat'
 
-def create_bd():
+
+def create_bd() -> tuple:
+    db_author = sqlite3.connect('quotes.db')
+    cur = db_author.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS authors(
+        AuthorID INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        surname TEXT,
+        age INTEGER);
+    """)
+    db_author.commit()
+
     db = sqlite3.connect('quotes.db')
     sql = db.cursor()
-    sql.execute("""CREATE TABLE IF NOT EXISTS users(
-    author TEXT,
-    quote TEXT
-    )""")
+    sql.execute("""CREATE TABLE IF NOT EXISTS users (
+        QuotesID INTEGER PRIMARY KEY AUTOINCREMENT,
+        quote TEXT,
+        AuthorID int,        
+        FOREIGN KEY (AuthorID) REFERENCES authors(AuthorID));    
+        """)
     db.commit()
-    return db, sql
+    return db, sql, db_author, cur
 
-def get_bd(id=None):
-    print(id)
-    db, sql = create_bd()
-    if not id:
-        sql.execute('SELECT rowid, author, quote FROM users')
-        return sql.fetchall()
-    else:    
-        sql.execute(f"SELECT rowid FROM users WHERE rowid = '{id}' ")
-        if not sql.fetchone():
-            print('Такая цитата не существует')
-            return None                
-        else:         
-            sql.execute(f"SELECT rowid, author, quote FROM users WHERE rowid = '{id}'")
-            return sql.fetchall()
-        
-    
 
+def get_bd():
+    _, sql, _, cur = create_bd()
+    sql.execute('SELECT rowid, author, quote FROM users')
     
-def post_bd(inp_author, inp_quote):
+    return sql.fetchall()
+
+
+def post_bd(inp_author, inp_quote) -> int:
     
-    db, sql = create_bd() 
+    db, sql, db_author, cur = create_bd()
     sql.execute(f"SELECT quote FROM users WHERE quote = '{inp_quote}' ")
     if not sql.fetchone():
-        sql.execute(f"INSERT INTO users VALUES ('{inp_author}', '{inp_quote}')")
+        sql.execute(f"INSERT INTO users(AuthorID, quote)  VALUES ('{inp_author}', '{inp_quote}')")
         db.commit()
     else: 
         print('Такая цитата существует')
@@ -47,7 +50,7 @@ def post_bd(inp_author, inp_quote):
 
 def put_bd(inp_author, inp_quote, id):
     
-    db, sql = create_bd()
+    db, sql, db_author, cur = create_bd()
     print(inp_author,inp_quote)
     sql.execute(f"SELECT quote FROM users WHERE rowid = '{id}' ")
     if not sql.fetchone():
@@ -61,15 +64,27 @@ def put_bd(inp_author, inp_quote, id):
     
     return sql.fetchall()
 
+
 def delete_bd(id):
     
-    db, sql = create_bd()
+    db, sql, db_author, cur = create_bd()
     sql.execute(f"DELETE FROM users WHERE rowid = '{id}' ")
     db.commit()
 
-#print(get_bd(input('Введите id для изменения ')))
-#print(getid_bd())
-#post_bd(input('Введите автора для реестрации '),input('Введите цитату для реестрации ' ))
-# put_bd(a, c, input('Введите id для изменения '))
-# delete_bd(input('Введите автора для удаления '))
-# print(get_bd())
+
+def post_bd_authors(name, surname, age) -> int:
+    db, sql, db_author, cur = create_bd()
+    cur.execute(f"SELECT name FROM authors WHERE name = '{name}' ")
+    if not cur.fetchone():
+        cur.execute(f"INSERT INTO authors(name, surname, age) VALUES ('{name}', '{surname}', '{age}')")
+        db_author.commit()
+    else:
+        print('Такая цитата существует')
+
+    cur.execute('SELECT * FROM authors')
+
+    return cur.fetchall()
+
+print(create_bd())
+print(post_bd_authors('lehfr', 'Klemberh', 23))
+print(post_bd(5, 'Hjllhsdvhdvjsbhsdfvdbhf sdfhj s hgl'))
